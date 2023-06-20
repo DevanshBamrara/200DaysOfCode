abf793b11e01ad3edc860c4623b4c5e27a4f9253
Devansh Bamrara <69037067+DevanshBamrara@users.noreply.github.com>
2023-06-20 10:45:02 +0530

    Day 140

---
+++ Day140/459. Repeated Substring Pattern.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string tmp=s+s;
+        if(tmp.substr(1,tmp.size()-2).find(s)==-1) return false;
+        return true;
+    }
+};
